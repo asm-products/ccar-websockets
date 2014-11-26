7328b543dd5192a74f513fc80c9efdb6b962a3e7
Dinkar Ganti <dinkar.ganti@gmail.com>
2014-11-26 00:29:23 -0500

    Basic connection test

--- client/Test.hx
+++ client/Test.hx
@@ -1,6 +1,47 @@
+import js.html.*;
+import js.html.Event;
 class Test {
+
 	
-	static function main() {
-		trace("Hello world");
+	var serverHost : String = "localhost";
+	var protocol : String = "ws";
+	var portNumber : Int = 3000;
+	var websocket : WebSocket;
+	function new (){
+		serverHost = "localhost";
+		protocol = "ws";
+		portNumber = 3000;
+		websocket = new WebSocket(connectionString());
+		websocket.onclose = onClose;
+		websocket.onerror = onError;
+		websocket.onmessage = onMessage;
+		websocket.onopen = onOpen;
+
+
 	}
+	function connectionString() : String {
+		return protocol + "://" + serverHost + ":" + portNumber;
+	}
+	static function main() {
+		var test : Test = new Test();
+		trace(test.connectionString());
+	}
+	public function onClose(ev: Event){
+		trace("Connection closed");
+
+	}
+	public  function onOpen(ev: Event){
+		trace("Connection opened");
+	}
+	public  function onMessage(ev: Event){
+		trace("Received " + ev);
+
+	}
+	public  function onError(ev : Event){
+		trace("Error " + ev);
+	}
+	public  function doSend(aMessage : String){
+		websocket.send(aMessage);
+	}
+
 }
