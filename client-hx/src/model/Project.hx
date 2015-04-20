7c4f44a1a7fd0fcd34a9433bffe8573c5af809cb
Dinkar Ganti <dinkar.ganti@gmail.com>
2015-04-19 20:20:40 -0400

    Project ui

---
+++ client-hx/src/model/Project.hx
@@ -0,0 +1,54 @@
+package model;
+import util.Util;
+import haxe.Json;
+import haxe.Utf8;
+import haxe.Timer;
+import js.html.Element;
+import haxe.ds.GenericStack;
+import js.html.Event;
+import js.html.CloseEvent;
+import js.html.MessageEvent;
+import js.html.WebSocket;
+import js.html.DOMCoreException;
+import js.html.DivElement;
+import js.html.Document;
+import js.html.File;
+import js.html.KeyboardEvent;
+import js.html.InputElement;
+import js.html.SelectElement;
+import js.html.OptionElement;
+import js.html.FileReader;
+import js.html.ImageElement;
+import model.Contact;
+import model.Login;
+import model.Person;
+import model.LoginStatus;
+import model.Command;
+import model.CommandType;
+import model.UserOperation;
+import util.Util;
+import util.Config;
+import js.Browser;
+import js.html.ButtonElement;
+import js.html.TextAreaElement;
+import promhx.Stream;
+import promhx.Promise;
+import massive.munit.TestRunner;
+using promhx.haxe.EventTools;
+import promhx.Deferred;
+
+
+class Project {
+	var crudType : String;
+	var identification : String;
+	var companyUniqueId : String;
+	var summary : String;
+	var details : String;
+	var preparedBy : String;
+	var startDate : Date;
+	var endDate : Date;
+	uploadedBy : String;
+	uploadTime : Date;
+	
+
+}
