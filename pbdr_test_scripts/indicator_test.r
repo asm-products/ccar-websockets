0862b002071ef1e2372d89b94cdd6358a4c74a1f
Dinkar Ganti <dinkar.ganti@gmail.com>
2015-06-07 01:17:35 -0400

    shapiro test

--- pbdr_test_scripts/indicator_test.r
+++ pbdr_test_scripts/indicator_test.r
@@ -1,9 +1,36 @@
 #Script to test the normality of data
 
-library(rjson)
 
-indicatorURL <- "http://www.smart-earn.com/gj/5/16"
-indicatorData <- readLines(indicatorURL)
-v <- fromJSON(indicatorData)
-d <- sapply(v, function(y) y[[2]])
-shapiro.test(as.numeric(as.character(d[-1])))
+# Given a url compute if the data
+# in the format time, value 
+# passes normality test
+library(rjson)
+library ( pbdMPI , quietly = TRUE )
+
+computeShapiroTest<- function(indicatorURL, index) {
+
+	indicatorData <- readLines(indicatorURL)
+	v <- fromJSON(indicatorData)
+	if(length(v) > 1) {
+		d <- sapply(v, function(y) y[[2]])
+		return(c(index, shapiro.test(as.numeric(as.character(d[-1])))))
+	}else {
+		print(sprintf("Ignoring %s",  indicatorURL))
+	}
+}
+
+init ()
+indicators <- c(1,5,6, 7,8, 9)
+indicators <- append(indicators, c(10:173))
+
+#indicators <- c(1)
+baseURL <- "http://www.smart-earn.com/gj/5/"
+indicatorURLs <- sapply(indicators, function(y) {
+		url <- sprintf("%s%d", baseURL, y)
+		index <- sprintf("%d", y)
+		computeShapiroTest(url, index)
+	}
+)
+
+indicatorURLs
+finalize()
