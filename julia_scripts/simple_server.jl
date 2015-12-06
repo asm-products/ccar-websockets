f8792e00f8471d0a2f8d41bfa05088298888e842
Dinkar Ganti <dinkar.ganti@gmail.com>
2015-12-05 23:03:49 -0500

    Some minor changes to julia script

--- julia_scripts/simple_server.jl
+++ julia_scripts/simple_server.jl
@@ -1,19 +1,25 @@
 
 portNumber = 20000
-server = listen(portNumber)
-while true
-  println("Waiting for connections on port : " * string(portNumber))
-  conn = accept(server)
-  println("Accepted connection :" * string(conn))
-  @async begin
-    try
-      while true
-        println("Waiting for text")
-        line = "Echo " * readline(conn)
-        write(conn,line)
+
+function server(aPortNumber)
+  server = listen(portNumber)
+  while true
+    println("Waiting for connections on port : " * string(portNumber))
+    conn = accept(server)
+    println("Accepted connection :" * string(conn))
+    @async begin
+      try
+        while true
+          println("Waiting for text")
+          line = "Echo " * readline(conn)
+          write(conn,line)
+        end
+      catch err
+        println("connection ended with error $err")
       end
-    catch err
-      println("connection ended with error $err")
     end
   end
 end
+
+
+server(portNumber)
